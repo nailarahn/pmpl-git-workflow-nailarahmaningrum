d592767b73c2f17eeace8fe6ae74beee38c66032
nailarahn <nailarahma35506@gmail.com>
2026-03-05 17:30:06 +0700

    feat: memperbarui pesan alert pada tombol

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Git Workflow Simulation Script
 
 function showMessage() {
-    alert("Praktikum Git Workflow berjalan.");
+    alert("Terima kasih telah mencoba simulasi Git Workflow pada praktikum PMPL Kelompok 3.");
 }
 
 console.log("Git Workflow Simulation loaded.");
