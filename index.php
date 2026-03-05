a33b928ca613375b33398a7ed6df58cea34e43ba
nailarahn <nailarahma35506@gmail.com>
2026-03-05 17:22:07 +0700

    feat: menambahkan deskripsi tambahan pada halaman utama

--- index.php
+++ index.php
@@ -17,6 +17,8 @@ $title = "Git Workflow Simulation";
 
     <p>Selamat datang di praktikum Git Workflow.</p>
 
+    <p>Halaman ini digunakan untuk mensimulasikan penggunaan Git dan GitHub dalam pengembangan perangkat lunak secara kolaboratif.</p>
+
     <button onclick="showMessage()">Klik Saya</button>
 
     <script src="script.js"></script>
